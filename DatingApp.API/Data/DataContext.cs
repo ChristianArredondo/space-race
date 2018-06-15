83aa3328b63ac672ff8dea6c5c5b4f8000f51de2
christianarredondo <christianarredondo@utexas.edu>
2018-06-15 09:05:31 -0500

    add user model, new migration, and update DataContext

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -1,6 +1,13 @@
 using DatingApp.API.Models;
 using Microsoft.EntityFrameworkCore;
 
+/*
+  - update any time a new model is created
+    - run `dotnet ef migrations add { NameOfMigration }` i.e. `dotnet ef migrations add AddedUserModel`
+    - confirm new table and columns by inspecting new file in /Migrations
+    - run `dotnet ef database update`
+*/
+
 namespace DatingApp.API.Data
 {
     public class DataContext : DbContext
@@ -8,5 +15,7 @@ namespace DatingApp.API.Data
         public DataContext(DbContextOptions<DataContext> options) : base(options) {}
 
         public DbSet<Value> Values { get; set; }
+
+        public DbSet<User> Users { get; set; }
     }
 }
