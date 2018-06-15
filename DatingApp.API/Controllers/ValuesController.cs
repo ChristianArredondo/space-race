910f3166c10451cc186409a2d59ba38ff98b45a4
christianarredondo <christianarredondo@utexas.edu>
2018-06-14 21:26:54 -0500

    update ValuesController endpoints `api/values` and `api/values/{id}` to use DataContext

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DatingApp.API.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DatingApp.API.Controllers
@@ -9,18 +10,31 @@ namespace DatingApp.API.Controllers
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
+
+      private readonly DataContext _context;
+      /* Constructor used to inject data context */
+      public ValuesController(DataContext context)
+      {
+       this._context = context;   
+      }
         // GET api/values
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IActionResult GetValues()
         {
-            return new string[] { "value1", "value2" };
+            /* `ToList()` = fetch values from db */
+            var values = this._context.Values.ToList();
+            /* Return values */
+            return Ok(values);
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public IActionResult GetValue(int id)
         {
-            return "value";
+            /* `FirstOrDefault()` returns first matched entity or null */
+            var value = this._context.Values.FirstOrDefault(val => val.Id == id);
+
+            return Ok(value);
         }
 
         // POST api/values
