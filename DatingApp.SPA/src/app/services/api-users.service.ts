8c7a5ae73de0269e895b916a3a688aa5c78b91aa
christianarredondo <christianarredondo@utexas.edu>
2018-06-17 14:49:46 -0500

    use environment for ApiUsersService `apiLocation`

--- DatingApp.SPA/src/app/services/api-users.service.ts
+++ DatingApp.SPA/src/app/services/api-users.service.ts
@@ -6,6 +6,8 @@ import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 // MODELS
 import { User } from '../models';
+// ENV
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -14,11 +16,12 @@ export class ApiUsersService {
   private _apiLocation: string;
 
   constructor(private _http: HttpClient) {
-    this._apiLocation = 'http://localhost:5000/api/users';
+    this._apiLocation = `${environment.apiUrl}/users`;
   }
 
   /** Fetches list of Users from server */
   get(token: string): Observable<User[]> {
+    // TODO. add HttpInterceptor for all reusable HttpHeaders-related workflows
     const accessToken = `Bearer ${token}`;
     const headers = new HttpHeaders({ Authorization: accessToken });
     return this._http.get<User[]>(this._apiLocation, { headers }).pipe(
